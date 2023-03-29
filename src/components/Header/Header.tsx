35c120507b6445915cac904d35355e8c82b9f0fe
Vlad Hapon <vladhpn@gmail.com>
2023-03-29 17:14:46 +0100

    main page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
-    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+    <nav className="bg-violet-500 px-4 lg:px-6 py-2.5">
+      <div className="flex flex-wrap justify-between items-center mx-auto container">
         <a href="https://flowbite.com" className="flex items-center">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,8 +1,32 @@
-
-
+import { MainInfo } from '../../components/MainInfo/MainInfo';
 
 const Main = () => {
-  return <h1>Main</h1>;
+  return (
+    <div className="container mx-auto">
+      <MainInfo />
+      {/* <div className=" p-4 w-[280px]">
+        <div className=" p-4 bg-white rounded-lg shadow dark:bg-gray-800 md:p-8">
+          <div className="mb-4 text-sm font-light text-gray-500 dark:text-gray-400">
+            <h3 className="mb-3 text-2xl font-bold text-gray-900 dark:text-white">
+              Вага
+            </h3>
+            <p className="text-xl  text-gray-900 dark:text-white">4 кг</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-4 w-[280px]">
+        <div className="p-4 bg-white rounded-lg shadow dark:bg-gray-800 md:p-8">
+          <div className="mb-4 text-sm font-light text-gray-500 dark:text-gray-400">
+            <h3 className="mb-3 text-2xl font-bold text-gray-900 dark:text-white">
+              Зріст
+            </h3>
+            <p className="text-xl text-gray-900 dark:text-white">30 см</p>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
 };
 
 export default Main;
